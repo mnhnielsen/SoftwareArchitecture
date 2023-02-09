3884d77fe3c009635b0b76d56af44d1aa87efea6
mnhnielsen <mnhnielsen@gmail.com>
2023-02-09 21:04:42 +0100

    Kafka exercies

---
+++ Lecture_2/exercises_lecture02/dotnet_consumer/dotnetConsumer/dotnetConsumer/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Confluent.Kafka;
+
+namespace KafkaConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // the bootstrap server is the address of the kafka broker, i.e. the docker container. Here you can specify multiple brokers 
+            // separated by a comma to enable load balancing and fault tolerance.
+            // The group id is used to identify the consumer group. All consumers with the same group id belong to the same consumer group.
+            // The auto offset reset defines the behavior of the consumer if no offset is stored for a partition. Here we set it to earliest
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = "kafka1:9092",
+                GroupId = "example-consumer-group",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+            string FileName = "the-rabbit-hole.txt";
+            File.WriteAllText(FileName, "CONSUMERTEXT" + Environment.NewLine);
+
+            using (var consumer = new ConsumerBuilder<Null, string>(config).Build())
+            {
+                consumer.Subscribe("TestTopic");
+
+                Console.WriteLine("Kafka Consumer is running...");
+
+                while (true)
+                {
+                    try
+                    {
+                        var message = consumer.Consume();
+
+                        Console.WriteLine($"Message received: '{message.Value}' from partition {message.Partition} with offset {message.Offset}");
+                        File.AppendAllText(FileName, message.Value);
+                    }
+                    catch (ConsumeException e)
+                    {
+                        Console.WriteLine($"Consumption failed: {e.Error.Reason}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Lecture_2/exercises_lecture02/dotnet_producer/dotnetProducer/dotnetProducer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Confluent.Kafka;
+
+namespace DotnetKafkaProducer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // the bootstrap server is the address of the kafka broker, i.e. the docker container. Here you can specify multiple brokers 
+            // separated by a comma to enable load balancing and fault tolerance.
+            var config = new ProducerConfig { BootstrapServers = "kafka1:9092" };
+            string aliceText = File.ReadAllText("alice-in-wonderland.txt");
+
+            string[] text = aliceText.Split(' ');
+
+            
+
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                Console.WriteLine("A kafka message");
+
+                for (int i = 0; i < text.Length; i++)
+                {
+                    try
+                    {
+                        var message = $"{text[i]} ";
+                        var result = producer.ProduceAsync("TestTopic", new Message<Null, string> { Value = message }).Result;
+
+                        Console.WriteLine($"Message '{message}' sent to partition {result.Partition} with offset {result.Offset}");
+                    }
+                    catch (ProduceException<Null, string> e)
+                    {
+                        Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+                    }
+                    System.Threading.Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
